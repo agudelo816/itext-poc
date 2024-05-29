fc5895cb3ca087743b0f0bd4159399a2c6146e47
Daniel Agudelo <d.agudelo816@gmai.com>
2024-05-28 20:28:24 -0400

    refactored

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,55 +12,45 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("MAIN MAIN");
-        // Path to the PDF file
+    public static void main(String[] args) throws Exception {
         String dest = "table_example.pdf";
-
-        // Creating a PdfWriter
-        PdfWriter writer = new PdfWriter(dest);
-
-        // Creating a PdfDocument
-        PdfDocument pdf = new PdfDocument(writer);
-
-        // Creating a Document
-        Document document = new Document(pdf);
-
-        // List of column names
-        List<String> columnNames = Arrays.asList("Column 1f", "Column 2f", "Column 3", "Column 4");
-        int numColumns = columnNames.size();
-
-        // Define column widths (each column width set to 25)
-        float[] columnWidths = new float[numColumns];
+        List<String> columnNames = Arrays.asList("Column 1", "Column 2", "Column 3");
+        float[] columnWidths = new float[columnNames.size()];
         Arrays.fill(columnWidths, 25);
 
-        // Define column widths (you can adjust the widths as per your requirements)
-//        float[] columnWidths = {25F, 25F, 25F, 25F};
+        PdfWriter writer = createPdfWriter(dest);
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
 
-        // Creating a table
-//        Table table = new Table(UnitValue.createPercentArray(columnWidths));
-        Table table = new Table(columnWidths);
-        table.setWidth(UnitValue.createPercentValue(100));
-
-        // Setting font and font size (example)
         com.itextpdf.kernel.font.PdfFont font = com.itextpdf.kernel.font.PdfFontFactory.createFont(com.itextpdf.io.font.constants.StandardFonts.HELVETICA);
+        Table table = createTable(columnWidths);
+        addHeaderCells(table, columnNames, font);
 
-        // Adding header cells to the table
+        document.add(table);
+        document.close();
+    }
+
+    private static PdfWriter createPdfWriter(String dest) throws Exception {
+        return new PdfWriter(dest);
+    }
+
+    private static Table createTable(float[] columnWidths) {
+        Table table = new Table(UnitValue.createPercentArray(columnWidths));
+        table.setWidth(UnitValue.createPercentValue(100));
+        return table;
+    }
+
+    private static void addHeaderCells(Table table, List<String> columnNames, com.itextpdf.kernel.font.PdfFont font) {
         for (String columnName : columnNames) {
             Paragraph paragraphCell = new Paragraph(columnName);
             paragraphCell.setFont(font);
             paragraphCell.setFontSize(10);
             paragraphCell.setTextAlignment(TextAlignment.CENTER);
-            Cell celda1 = new Cell();
-            celda1.add(paragraphCell);
-            table.addHeaderCell(celda1);
+            Cell cell = new Cell();
+            cell.add(paragraphCell);
+            table.addHeaderCell(cell);
         }
-
-        // Adding the table to the document
-        document.add(table);
-
-        // Closing the document
-        document.close();
     }
 }
